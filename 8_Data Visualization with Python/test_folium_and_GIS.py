298cabad75760bf883298987e89164478fc03c91
Fran Hsu <fran@Frans-MacBook-Air.local>
2026-03-11 17:23:35 -0400

    feat: finally finished course 8 Data Visualization with Python

---
+++ 8_Data Visualization with Python/test_folium_and_GIS.py
@@ -0,0 +1,13 @@
+import numpy as np  # useful for many scientific computing in Python
+import pandas as pd # primary data structure library
+
+import folium
+
+print('Folium installed and imported!')
+
+
+# define the world map
+world_map = folium.Map()
+
+# display world map
+world_map
